ccb406e74c405ce1a00ac596d4b719fb7b2b4b42
Yohan Vinu <yohanvvinu@gmail.com>
2023-03-23 20:36:10 +0530

    Add requirements.txt

--- main.py
+++ main.py
@@ -48,42 +48,42 @@ if place:
     except KeyError:
         st.write("You entered a place that does not exist!")
 
-# hide_streamlit_style = """
-#                 <style>
-#                 div[data-testid="stToolbar"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 div[data-testid="stDecoration"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 div[data-testid="stStatusWidget"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 #MainMenu {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 header {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 footer {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 footer:after{
-#                 visibility: visible;
-#                 content: 'Made by Yohan Vinu';
-#                 display:block;
-#                 position:relative;
-#                 color:grey
-#                 }
-#                 </style>
-#                 """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer:after{
+                visibility: visible;
+                content: 'Made by Yohan Vinu';
+                display:block;
+                position:relative;
+                color:grey
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
